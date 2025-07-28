f09633b99a186fb9986e421dc8ce01b36a9c966b
AditiPawar7 <aditipawar6122004@gmail.com>
2025-07-28 20:26:37 +0530

    Add files via upload

---
+++ Basic - IO/Swap_Two_Number_Without_Temp.java
@@ -0,0 +1,29 @@
+import java.lang.*;
+import java.util.*;
+
+public class Swap_Two_Number_Without_Temp
+{
+public static void main(String[] args)
+{
+	  int No1 = 0, No2 = 0;
+	  Scanner sc = new Scanner(System.in);
+	
+	  System.out.print("\n Enter 1st Number : ");
+	  No1 = sc.nextInt();
+	
+	  System.out.print("\n Enter 2nd Number : ");
+	  No2 = sc.nextInt();
+	
+      
+	  System.out.println("\n Before Swapping => " + "\t 1st Number = " + No1 + "\t 2nd Number = " + No2);
+    
+
+      No1 = No1 + No2;
+      No2 = No1 - No2;
+	  No1 = No1 - No2;
+	
+	  System.out.println("\n After Swapping => " + "\t 1st Number = " + No1 + "\t 2nd Number = " + No2);
+   
+   }
+}
+	

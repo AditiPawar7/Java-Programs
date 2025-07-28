1220043bbdb98543d46c824e3a172f37d80532c5
AditiPawar7 <aditipawar6122004@gmail.com>
2025-07-28 20:29:58 +0530

    Update Swap_Two_Number.java

--- Basic - IO/Swap_Two_Number.java
+++ Basic - IO/Swap_Two_Number.java
@@ -6,7 +6,7 @@ public class Swap_Two_Number
 public static void main(String[] args)
 {
 	  int No1 = 0, No2 = 0, Temp = 0;
-      Scanner sc = new Scanner(System.in);
+          Scanner sc = new Scanner(System.in);
 	
 	  System.out.print("\n Enter 1st Number : ");
 	  No1 = sc.nextInt();
@@ -14,11 +14,11 @@ public static void main(String[] args)
 	  System.out.print("\n Enter 2nd Number : ");
 	  No2 = sc.nextInt();
 	
-      System.out.println("\n Before Swapping => " + "\t 1st Number = " + No1 + "\t 2nd Number = " + No2);
+          System.out.println("\n Before Swapping => " + "\t 1st Number = " + No1 + "\t 2nd Number = " + No2);
     
 
-      Temp = No1;
-      No1  = No2;
+          Temp = No1;
+          No1  = No2;
 	  No2  = Temp;
 	
 	  System.out.println("\n After Swapping => " + "\t 1st Number = " + No1 + "\t 2nd Number = " + No2);
